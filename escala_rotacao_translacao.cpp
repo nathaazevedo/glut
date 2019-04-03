532858a050970345796268dad5ede44a27cd45c4
Nathã <31596001+nathaazevedo@users.noreply.github.com>
2019-04-03 04:26:20 -0300

    Update and rename escala_rotacao_translacao to escala_rotacao_translacao.cpp

--- escala_rotacao_translacao.cpp
+++ escala_rotacao_translacao.cpp
@@ -12,19 +12,20 @@ void display(void);
 void tela(GLsizei w, GLsizei h);
 void keyboard(unsigned char tecla, int x, int y);
 
-int main(int argc, char** argv) {
-	glutInit(&argc, argv); // suporte a janelas
+int main(int argc, char** argv){
+	glutInit(&argc, argv);  // controla se o sistema operacional tem suporte a janelas.
 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // padrão de cores
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);  // quantidade de buffer de cores e que o padrao de cores é RGB ou RGBA
 
-	glutInitWindowSize(janela_largura, janela_altura); // tamanho da janela
-	glutInitWindowPosition(100, 100); // posicao que surge a janela
-	glutCreateWindow("rotacao"); // cria janela
+	glutInitWindowSize(janela_largura, janela_altura);  // tamanho da janela
+	glutInitWindowPosition(300, 300); // posicao inicial da janela
 
-	glutReshapeFunc(tela); // configura a tela
+	glutCreateWindow("GLUT Rotacao/Zoom/Translacao ---- exercicio de glut");   /// cria a janela
+
+	glutReshapeFunc(tela); // configura tela
 	glutDisplayFunc(display);
 	glutKeyboardFunc(&keyboard); // chama o teclado
-	glutMainLoop(); // redesenhar
+	glutMainLoop(); // Redesenhar
 
 	return(0);
 }
@@ -59,10 +60,10 @@ void desenhar() {
 	glRotated(rotacao, 0, 0, 1);
 
 	glBegin(GL_TRIANGLES);
-		glColor3f(1.0, 0.0, 0.0); // Cor
-		glVertex2f(-100, -100);
-		glVertex2f(0, 0);
-		glVertex2f(100, -100);
+	glColor3f(1.0, 0.0, 0.0); // Cor
+	glVertex2f(-100, -100);
+	glVertex2f(0, 0);
+	glVertex2f(100, -100);
 	glEnd();
 
 	glRotated(-rotacao, 0, 0, 1); // sem rotacao
@@ -71,11 +72,11 @@ void desenhar() {
 	glTranslatef(trans, trans, 0);
 
 	glBegin(GL_QUADS);
-		glColor3f(1.0, 0.0, 0.0); // Cor
-		glVertex2f(-30, 30);
-		glVertex2f(-10, 30);
-		glVertex2f(-10, 50);
-		glVertex2f(-30, 50);
+	glColor3f(1.0, 0.0, 0.0); // Cor
+	glVertex2f(-30, 30);
+	glVertex2f(-10, 30);
+	glVertex2f(-10, 50);
+	glVertex2f(-30, 50);
 	glEnd();
 
 	glScalef(1, 1, 0);
@@ -95,7 +96,7 @@ void display() {
 	glViewport(0, 0, janela_largura, janela_altura);
 	desenhar();
 
-	glFlush; // execute o desenho
+	glFlush(); // execute o desenho
 }
 
 void tela(GLsizei w, GLsizei h) {
