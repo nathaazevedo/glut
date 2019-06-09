33fbabb3467c05811d3e6746a3e9085a2068daf8
Nathã <31596001+nathaazevedo@users.noreply.github.com>
2019-06-09 20:02:05 -0300

    Update PongGame.cpp

--- PongGame.cpp
+++ PongGame.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
+#include <time.h>
 
 #define PI 3.1415926535898
 
@@ -16,7 +17,7 @@ GLfloat janela_x = (janela_largura / 2) - 5;
 GLfloat janela_y = (janela_altura / 2) - 5;
 
 // velocidade do jogo
-int vel_game = 5;
+int vel_game = 3;
 // Variaveis com relacao ao jogador
 GLfloat player1_posX = -janela_x + 20;
 GLfloat player1_posY = 0;
@@ -28,10 +29,14 @@ float ball_y = 0;
 // Numero de passos movidos por Loop da animacao
 float xStep = 1;
 float yStep = 1;
+// buffer para teclado
+bool* keyStates = new bool[256];
 
 void display(void);
 void tela(GLsizei w, GLsizei h);
-void keyboard(unsigned char tecla, int x, int y);
+void keyPressed(unsigned char key, int x, int y);
+void keyUp(unsigned char key, int x, int y);
+void keyOperations(void);
 void desenhar();
 void players(GLfloat pos_x, GLfloat pos_y);
 void bola(GLfloat pos_x, GLfloat  pos_y);
@@ -39,7 +44,9 @@ void animar_bola(int valor);
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);  // controla se o sistema operacional tem suporte a janelas.
-
+	for (int i = 0; i < 256; i++) {
+		keyStates[i] = false;
+	}
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);  // quantidade de buffer de cores e que o padrao de cores é RGB ou RGBA
 
 	glutInitWindowSize(janela_largura, janela_altura);  // tamanho da janela
@@ -49,60 +56,69 @@ int main(int argc, char** argv) {
 
 	glutReshapeFunc(tela); // configura tela
 	glutDisplayFunc(display);
-	glutKeyboardFunc(&keyboard); // chama o teclado
-	glutTimerFunc(2*vel_game, animar_bola, 1); // chama animacao
-	glutMainLoop(); // Redesenhar
 
+	glutKeyboardFunc(keyPressed); // chama o "keyPressed" quando uma tecla for pressionada 
+	glutKeyboardUpFunc(keyUp); // chama o "keyUp" quando uma tecla for solta
+
+	glutTimerFunc(4*vel_game, animar_bola, 1); // chama animacao
+
+	glutMainLoop(); // Redesenhar
+	 
 	return(0);
 }
 
-// Comandos do teclado
-void keyboard(unsigned char tecla, int x, int y) {
+void keyPressed(unsigned char key, int x, int y) {
+	keyStates[key] = true; // Set the state of the current key to pressed  
+}
 
-	if (tecla == 'w') {
-		if (player1_posY < janela_y - player_altura/2 - 10) {
-			player1_posY += vel_game;
+void keyUp(unsigned char key, int x, int y) {
+	keyStates[key] = false; // Set the state of the current key to not pressed  
+}
+
+void keyOperations(void) {
+	if (clock() > 3000) {
+		if (keyStates['w']) {
+			if (player1_posY < janela_y - player_altura / 2 - 10) {
+				player1_posY += vel_game;
+			}
+		} else if (keyStates['s']) {
+			if (player1_posY > -janela_y + player_altura / 2 + 10) {
+				player1_posY -= vel_game;
+			}
+		}
+		if (keyStates['o']) {
+			if (player2_posY < janela_y - player_altura / 2 - 10) {
+				player2_posY += vel_game;
+			}
+		} else if (keyStates['l']) {
+			if (player2_posY > -janela_y + player_altura / 2 + 10) {
+				player2_posY -= vel_game;
+			}
 		}
 	}
-	if (tecla == 's') {
-		if (player1_posY > -janela_y + player_altura/2 + 10) {
-			player1_posY -= vel_game;
-		}
-	}
-	if (tecla == 'o') {
-		if (player2_posY < janela_y - player_altura/2 - 10) {
-			player2_posY += vel_game;
-		}
-	}
-	if (tecla == 'l') {
-		if (player2_posY > -janela_y + player_altura/2 + 10) {
-			player2_posY -= vel_game;
-		}
-	}
-	glutPostRedisplay(); // atualizar tela
 }
 
 void desenhar() {
-	// Grossura das linhas
-	glLineWidth(7);
-	glColor3ub(255, 255, 255); // itens abaixo na cor branca
-	// Limites da tela
-	glBegin(GL_LINE_LOOP);
-	glVertex2f(-janela_x, janela_y);
-	glVertex2f( janela_x, janela_y);
-	glVertex2f( janela_x, -janela_y);
-	glVertex2f(-janela_x, -janela_y);
-	glEnd(); // Fim quadrado
-	// Linha central
-	glBegin(GL_LINES);
-	glVertex2f(0, janela_y); // Primeiro ponto
-	glVertex2f(0, -janela_y); // Segundo ponto
-	glEnd();
-	// Players
-	players(player1_posX, player1_posY);  // player 1 - w - s
-	players(player2_posX, player2_posY);  // player 2 - o - l
-	// Bola
-	bola(ball_x, ball_y);
+		// Grossura das linhas
+		glLineWidth(7);
+		glColor3ub(255, 255, 255); // itens abaixo na cor branca
+		// Limites da tela
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(-janela_x, janela_y);
+		glVertex2f(janela_x, janela_y);
+		glVertex2f(janela_x, -janela_y);
+		glVertex2f(-janela_x, -janela_y);
+		glEnd(); // Fim quadrado
+		// Linha central
+		glBegin(GL_LINES);
+		glVertex2f(0, janela_y); // Primeiro ponto
+		glVertex2f(0, -janela_y); // Segundo ponto
+		glEnd();
+		// Players
+		players(player1_posX, player1_posY);  // player 1 - w - s
+		players(player2_posX, player2_posY);  // player 2 - o - l
+		// Bola
+		bola(ball_x, ball_y);
 }
 
 void players(GLfloat pos_x, GLfloat  pos_y) {
@@ -129,25 +145,27 @@ void bola(GLfloat pos_x, GLfloat  pos_y) {
 }
 
 void animar_bola(int valor) {
-	// Limites de tela superior e inferior
-	if ((ball_y > janela_y - 10) || (ball_y < -janela_y + 10)) {
-		yStep = -yStep;
-	}
-	// Quando um player marcar gol, centralizar a bola
-	if ((ball_x > janela_x) || (ball_x < -janela_x)) {
-		ball_x = 0;
-	}
-	// Se colidir com algum player, mudar a direcao da bol
-	if ((ball_y < player1_posY + player_altura/2 && ball_y > player1_posY - player_altura / 2 && ball_x == player1_posX + 10) ||
-		(ball_y < player2_posY + player_altura / 2 && ball_y > player2_posY - player_altura / 2 && ball_x == player2_posX - 10 )) {
-		xStep = -xStep;
-	}
+	if(clock() > 3000){
+		// Limites de tela superior e inferior
+		if ((ball_y > janela_y - 10) || (ball_y < -janela_y + 10)) {
+			yStep = -yStep;
+		}
+		// Quando um player marcar gol, centralizar a bola
+		if ((ball_x > janela_x) || (ball_x < -janela_x)) {
+			ball_x = 0;
+		}
+		// Se colidir com algum player, mudar a direcao da bol
+		if ((ball_y < player1_posY + player_altura / 2 && ball_y > player1_posY - player_altura / 2 && ball_x == player1_posX + 10) ||
+			(ball_y < player2_posY + player_altura / 2 && ball_y > player2_posY - player_altura / 2 && ball_x == player2_posX - 10)) {
+			xStep = -xStep;
+		}
 
-	ball_x += xStep;
-	ball_y += yStep;
+		ball_x += xStep;
+		ball_y += yStep;
 
+	}
 	glutPostRedisplay();
-	glutTimerFunc(2*vel_game, animar_bola, 1);
+	glutTimerFunc(2 * vel_game, animar_bola, 1);
 }
 
 void display() {
@@ -161,6 +179,7 @@ void display() {
 
 	glViewport(0, 0, janela_largura, janela_altura);
 
+	keyOperations();
 	desenhar();
 
 	glFlush(); // executa o desenho
@@ -175,3 +194,8 @@ void tela(GLsizei w, GLsizei h) {
 
 	glMatrixMode(GL_MODELVIEW);
 }
+
+/*
+	MultiKey on keyboard:
+	http://www.swiftless.com/tutorials/opengl/keyboard.html
+*/
